6dfc14765cdbed6f26b9a0a643bccc7bb8bd6c52
JessyXie <shanshan.xie96@gmail.com>
2019-04-20 13:31:59 -0400

    added state machine

--- executive_smach/smach_ros/test/test.py
+++ executive_smach/smach_ros/test/test.py
@@ -5,15 +5,24 @@ from smach_ros.srv import *
 import time
 import smach
 import smach_ros
-
+from std_msgs.msg import String
+from smach_ros.msg import *
 cur_path = []
+global reached 
 reached = False
+global ready_to_go 
+ready_to_go= False
+global transit_count
 # define state Foo
+global attable_count
+pub = rospy.Publisher('robot_state', robot_state, queue_size=10)
 class Base(smach.State):
     def __init__(self):
         smach.State.__init__(self, outcomes=['Loading','Ready'])
 
     def execute(self, userdata):
+        cur_state = robot_state()
+        cur_state.state = "AtBase"
         rospy.loginfo('Executing state Base')
         rospy.wait_for_service('Full_Path')
         try:
@@ -21,7 +30,9 @@ class Base(smach.State):
             resp = full_path_service()
             global cur_path
             cur_path = list(resp.path)
-            print(cur_path)
+            cur_state.next_goal = cur_path[0]
+            pub.publish(cur_state)
+            #print(cur_path)
             return "Ready"
         except rospy.ServiceException, e:
             print "Service call failed: %s"%e
@@ -37,37 +48,61 @@ class InTransit(smach.State):
     def execute(self, userdata):
         rospy.loginfo('Executing state InTransit')
         global reached
+        global transit_count
+        transit_count = 0        
+        rospy.Subscriber("Transit_status", String, self.callback)
+        cur_state = robot_state()
+        cur_state.state = "InTransit"
+        cur_state.next_goal = cur_path[0]
         if reached == True:
+            transit_count = 1
             reached = False
             return 'reached'
         else:
-            time.sleep(0.5)
+            pub.publish(cur_state)
+            time.sleep(0.1)
             return 'wait'
+    def callback(self,data):
+        global reached
+        global transit_count
+        if data.data == "Done" and transit_count ==0:
+            reached = True
 
 class AtTable(smach.State):
     def __init__(self):
-        smach.State.__init__(self, outcomes=['Done','finished'])
+        smach.State.__init__(self, outcomes=['wait','Done','finished'])
 
     def execute(self, userdata):
         rospy.loginfo('Executing state AtTable')
+        rospy.Subscriber("serve_status", String, self.callback)
+        global attable_count
+        global reached
+        global ready_to_go
         global cur_path
+        cur_state = robot_state()
+        cur_state.state = "AtTable"
+        if len(cur_path) >1:
+            cur_state.next_goal = cur_path[0]
+        else:
+            cur_path.next_goal = -1
+        pub.publish(cur_state)
+        attable_count = 0
+        if ready_to_go == False:
+            time.sleep(0.1)
+            return 'wait'
+        attable_count = 1
+        ready_to_go = False
         print(cur_path[0])
         if cur_path[0]==1:
             return 'finished'
         else:
+
             cur_path.pop(0)
-            return 'Done'    
-
-def Reach_Table(request):
-    global reached
-    reached = True
-    return reach_tableResponse()
-
-def reach_table_server():
-    #rospy.init_node('reach_table_server')
-    s = rospy.Service('reach_table', reach_table, Reach_Table)
-    rospy.spin()
-
+            return 'Done'
+    def callback(self,data):
+        global ready_to_go
+        if data.data == "Done" and attable_count == 0:
+            ready_to_go = True    
 
 
 def main():
@@ -84,8 +119,8 @@ def main():
         smach.StateMachine.add('InTransit', InTransit(), 
                                transitions={'reached':'AtTable', 'wait':'InTransit'})
         smach.StateMachine.add('AtTable', AtTable(), 
-                               transitions={'Done':'InTransit','finished':'outcome4'})
-    reach_table_server()
+                               transitions={'wait':'AtTable','Done':'InTransit','finished':'outcome4'})
+    #reach_table_server()
     # Execute SMACH plan
     outcome = sm.execute()
     
